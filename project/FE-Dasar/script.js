98b39cd6a63b035dbcf3e3bd123b03fea5c35863
Ilham Oktavian <99379666+Ilhamokta74@users.noreply.github.com>
2024-09-11 16:17:25 +0700

    fix

--- project/FE-Dasar/script.js
+++ project/FE-Dasar/script.js
@@ -6,11 +6,9 @@ var index = 0;
 
 nextSlide.onclick = function () {
   next("next");
-  console.log(`next ${slides.length}`);
 };
 prevSlide.onclick = function () {
   next("prev");
-  console.log(`prev ${slides.length}`);
 };
 
 function next(direction) {
@@ -19,19 +17,16 @@ function next(direction) {
     if (index == totalSlides) {
       index = 0;
     }
-    console.log(index);
   } else {
     if (index == 0) {
       index = totalSlides - 1;
     } else {
       index--;
     }
-    console.log(index);
   }
 
   for (i = 0; i < slides.length; i++) {
     slides[i].classList.remove("active");
-    console.log(`remove slides`)
   }
   slides[index].classList.add("active");
 }
